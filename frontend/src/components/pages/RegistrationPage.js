ec9d5fb3f5700586e7c94de5b1c7a21099b8430b
Meredith Zhang <90161936+edithzxin@users.noreply.github.com>
2022-07-17 13:19:15 +1000

    added a 'registration field' as a flex child to the registration page, + basic potential registration field options..?

--- frontend/src/components/pages/RegistrationPage.js
+++ frontend/src/components/pages/RegistrationPage.js
@@ -1,5 +1,9 @@
 export default function RegistrationPage() {
     return <div className="content">
         <h1>Registration</h1>
+        <input autocomplete="off" autofocus class="registration-field" name="username" placeholder="Username" type="text"/>
+        <input class="registration-field" name="password" placeholder="Password" type="password"/>
+        <input class="registration-field" name="confirmation" placeholder="Confirm Password" type="password"/>
+        <button class="btn btn-primary" type="submit">Register</button>
     </div>
 }
